eaa503e7ab4fafac60b7a4a007e41602510450f1
junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-04-05 06:50:26 -0700

    fix error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,41 +1,32 @@
 #include "lists.h"
 
 /**
- * print list int
+ * print_listint - a function that prints all elements of a list
  *
- *@h pointer to first node
+ * @h: pointer to first node
  *
- * return tye number of nodes
- */
-
+ * Return: the number of nodes
+*/
 size_t print_listint(const listint_t *h)
-
 {
 	size_t node_number = 1;
 
-	/* zero when h is null*/
-	/* if statement that test condition for h*/
-
+	/* 0 of nodes when h is null*/
 	if (h == NULL)
 		return (0);
 
-
 	while (h->next != NULL)
-
 	{
-
 		printf("%d\n", h->n);
-		/* is a pointer*/
 
-
+		/*change next node*/
 		h = h->next;
-		/* counting*/
+		/*count no of nodes*/
 		node_number++;
 	}
-	/*show last nod*/
 
-	printf("d\n", h->);
+	/*show node*/
+	printf("%d\n", h->n);
 
 	return (node_number);
-
 }
