694d749281316f3ceeed81fac4a1fe2264a663b8
junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-04-05 07:45:39 -0700

    third task

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - funtion for new node as head
+ *
+ * @head: pointer 
+ *
+ * @n: integer n to add in a new node
+ *
+ * Return: address of the new element or NULL if it fails
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newbie;
+
+	new_node = malloc(sizeof(listint_t));
+	if (newbie == NULL)
+		return (NULL);
+
+	/*link first node in head with the new_node*/
+	if (*head == NULL)
+		newbie->next = NULL;
+	else
+		newbie->next = *head;
+
+	newbie->n = n;
+	/*put new node at the beginning of the list*/
+	*head = newbie;
+
+	return (*head);
+}
