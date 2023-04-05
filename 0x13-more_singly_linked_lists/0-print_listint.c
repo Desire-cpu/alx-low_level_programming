f837b5098702f197a97dfa78f2960558d0ab195e
junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-04-05 06:57:50 -0700

    fixing samr error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,23 @@
 #include "lists.h"
 
 /**
- * print_listint - a function that prints all elements of a list
+ * print_listint - elements of a list.
  *
- * @h: pointer to first node
+ * @h: poinyer to header.
  *
- * Return: the number of nodes
-*/
+ * Return: numbers of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_number = 1;
 
-	/* 0 of nodes when h is null*/
-	if (h == NULL)
-		return (0);
+	size_t nudo_size = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-
-		/*change next node*/
 		h = h->next;
-		/*count no of nodes*/
-		node_number++;
+		nudo_size++;
 	}
+	return (nudo_size);
 
-	/*show node*/
-	printf("%d\n", h->n);
-
-	return (node_number);
 }
