1d5a8c607400b79395a831039b26630f1bae5021
Junior sibusiso ngondo <67375679+Desire-cpu@users.noreply.github.com>
2023-04-05 22:16:12 -0700

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,25 +5,32 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * singly list
+ * struct listint_s
+ * @n: integer 
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description:singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
+
 /**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
+ * singly list
+ * struct listint_s
+ * @n: integer 
  * @next: points to the next node
  *
- * Description: singly linked list of pointers
+ * Description:singly linked list node structure
  */
+
+
+
 typedef struct listp_s
 {
 	void *p;
